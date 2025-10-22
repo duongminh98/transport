e5734367f72c2b3e2cdb9e9bc42cc1848170694f
duongminh98 <duongtuanminh2005@gmail.com>
2025-10-22 16:22:03 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,7 @@ function App() {
   const { messages, isLoading, sendMessage, clearChat } = useChat(selectedModel);
 
   // Get the last user message (question)
-  const lastUserMessage = React.useMemo(() => {
-    for (let i = messages.length - 1; i >= 0; i--) {
-      if (messages[i].role === 'user') {
-        return messages[i];
-      }
-    }
-    return undefined;
-  }, [messages]);
+  
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-900 via-purple-900 to-indigo-900">
